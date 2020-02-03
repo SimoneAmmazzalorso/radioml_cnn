a138a4d1ec80a14714d06469edb88619dd2291cd
Simone Ammazzalorso <simone.ammazzalorso@unito.it>
2020-02-03 15:59:26 +0100

    added fixed pixel for cropper

--- dataset_generator.py
+++ dataset_generator.py
@@ -16,6 +16,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--overwrite", action='store_true', help="overwrite maps.")
 parser.add_argument("--xsize_old", type=int, help="xsize of the original tif figure.", default=2000)
 parser.add_argument("--xsize_new", type=int, help="xsize of the new tif figure.", default=200)
+parser.add_argument("--fix_x", type=int, help="fix x pixel.", default=-1)
+parser.add_argument("--fix_y", type=int, help="fix y pixel.", default=-1)
 parser.add_argument("--path", type=str, help="path of source files.", default='')
 parser.add_argument("--path_dest", type=str, help="path of destination files.", default='')
 parser.add_argument("--tag", type=str, help="tag of destination files.", default='')
@@ -24,6 +26,8 @@ args = parser.parse_args()
 overwrite = args.overwrite
 x_size_old = args.xsize_old
 x_size_new = args.xsize_new
+fix_x = args.fix_x
+fix_y = args.fix_y
 path = args.path
 destinationPath_data = args.path_dest
 tag = args.tag
@@ -52,8 +56,15 @@ t_start = time.time()
 for num, path_ in enumerate(all_images):
     if not os.path.exists(destinationPath_data+'msim_'+tag+'%04i_data.tif'%num) or overwrite:
         image_data = np.array(Image.open(path_))
-        (x_index,y_index)=(np.random.choice(x_index_list,1)[0],np.random.choice(y_index_list,1)[0])
-        print(x_index,x_index+x_size_new)
+        if fix_x != -1 and fix_x > 0 and fix_x <= x_index_list[-1]:
+            x_index = fix_x
+        else:
+            x_index = np.random.choice(x_index_list,1)[0]
+        if fix_y != -1 and fix_y > 0 and fix_y <= y_index_list[-1]:
+            y_index = fix_y
+        else:
+            y_index = np.random.choice(y_index_list,1)[0]
+        print('pixel coordinates:',x_index,y_index)
         new_image = image_data[y_index:y_index+y_size_new,x_index:x_index+x_size_new]
         #print(new_image.shape)
         #plt.imshow(new_image)
--- script_cropper.sh
+++ script_cropper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source activate tensorflow
-conda list -n tensorflow
+#conda list -n tensorflow
 conda info -e
 
-python /archive/home/sammazza/radioML/dataset_generator.py --tag=CNN_1 --path=/archive/home/sammazza/radioML/data/mapsim_PS/ --path_dest=/archive/home/sammazza/radioML/data/mapsim_PS_reduced/ 
+python /archive/home/sammazza/radioML/dataset_generator.py --tag=CNN_GAL --fix_y=450 --path=/archive/home/sammazza/radioML/data/mapsim_PS/ --path_dest=/archive/home/sammazza/radioML/data/mapsim_PS_GAL/
